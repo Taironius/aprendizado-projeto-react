9b8e7052cf1ab262a754ef3e27886f52791276d0
Kenedy Jardim <kenedydacosta@gmail.com>
2023-11-25 15:50:58 -0300

    finalizado parte das medias

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -5,12 +5,31 @@ const Formulario = () => {
     let [materiaB, setMateriaB] = useState(0);
     let [materiaC, setMateriaC] = useState(0);
 
+    const renderizaResultado = () => {
+        const soma = materiaA + materiaB + materiaC;
+        const media = soma / 3;
+
+        if(media >= 7){
+            return (
+                <p>Você foi aprovado</p>
+            )
+        }else {
+            return (
+                <p>Você não foi aprovado</p>
+            )
+        }
+    }
+
     return (
         <form>
-            <input type="number" placeholder="Nota da Materia A" onChange={evento => setMateriaA(evento.target.value)} />
-            <input type="number" placeholder="Nota da Materia B" onChange={evento => setMateriaB(evento.target.value)} />
-            <input type="number" placeholder="Nota da Materia C" onChange={evento => setMateriaC(evento.target.value)}/>
-            <p>O aluno foi aprovado</p>
+            <input type="number" placeholder="Nota da Materia A" onChange={evento => setMateriaA(parseInt(evento.target.value))} />
+            <input type="number" placeholder="Nota da Materia B" onChange={evento => setMateriaB(parseInt(evento.target.value))} />
+            <input type="number" placeholder="Nota da Materia C" onChange={evento => setMateriaC(parseInt(evento.target.value))}/>
+            {/* <p>O aluno foi aprovado</p> */}
+            {renderizaResultado()}
+            {materiaA}
+            {materiaB}
+            {materiaC}
         </form>
     )
 }
